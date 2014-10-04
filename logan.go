016dc307c5505788bf68edf48d4daaa4968bc44f
First-developer <lioninho11@hotmail.fr>
2014-10-04 09:09:56 +0200

    Added : Docs directory and initial design doc page

--- logan.go
+++ logan.go
@@ -1,2 +1,2 @@
-package logan
+package main
 
--- logan_test.go
+++ logan_test.go
@@ -1,9 +1,9 @@
 package logan_test
 
 import (
-	//. "github.com/fdsolutions/logan"
+	. "github.com/fdsolutions/logan"
 
-	//. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
