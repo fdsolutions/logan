599e001842984f593da35e738d32bb4b12cf92f4
First-developer <lioninho11@hotmail.fr>
2015-04-01 01:14:02 +0200

    - Code : Add file helper functions

---
+++ helpers/file_helper.go
@@ -0,0 +1,21 @@
+package helpers
+
+import (
+	"os"
+)
+
+// DoesFileExist checks whether or not a file exist at the given path
+// TODO: Returns an error if any, this to enable error handling if needed
+func DoesFileExist(path string) (doesExist bool) {
+	doesExist = true
+	_, err := os.Stat(path)
+	if err != nil && os.IsNotExist(err) {
+		doesExist = false
+	}
+	return doesExist
+}
+
+// FileDoesntExist a handy function for the opposite of FileDoesExist
+func FileDoesntExist(path string) bool {
+	return !DoesFileExist(path)
+}
